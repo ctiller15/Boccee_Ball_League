40beb1a6ba0583b4a10510bc843d3ac60c772e2b
Tiller <ctiller@proforma.com>
2018-04-17 13:13:57 -0400

    first commit

---
+++ Game.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boccee_Ball_League
+{
+    class Game
+    {
+        public int ID { get; set; }
+
+        // many to many.
+        public Team HomeTeam { get; set; }
+        public Team AwayTeam { get; set; }
+
+        public int HomeScore { get; set; }
+        public int AwayScore { get; set; }
+        public DateTime DateHappened { get; set; }
+        public string Notes { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boccee_Ball_League
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
