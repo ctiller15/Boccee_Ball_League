83e7c631bdfc85bd3920f8d75b6f2fd40605692b
Tiller <ctiller@proforma.com>
2018-04-17 15:09:33 -0400

    Querydatabase to show all teams

--- Program.cs
+++ Program.cs
@@ -13,18 +13,19 @@ namespace Boccee_Ball_League
         static void Main(string[] args)
         {
             var db = new BocceeBallContext();
-            var team1 = db.Teams.Single(t => t.ID == 2);
-            var team2 = db.Teams.Single(t => t.ID == 3);
+            var Teams = db.Teams;
+            //var team1 = db.Teams.Single(t => t.ID == 2);
+            //var team2 = db.Teams.Single(t => t.ID == 3);
 
-            var firstGame = new Models.Game
-            {
-                HomeTeamID = team1.ID,
-                AwayTeamID = team2.ID,
-                HomeScore = 10,
-                AwayScore = 20,
-                DateHappened = DateTime.Now,
-                Notes = "The away team kicked butt!!!",
-            };
+            //var firstGame = new Models.Game
+            //{
+            //    HomeTeamID = team1.ID,
+            //    AwayTeamID = team2.ID,
+            //    HomeScore = 10,
+            //    AwayScore = 20,
+            //    DateHappened = DateTime.Now,
+            //    Notes = "The away team kicked butt!!!",
+            //};
             //var secondGame = new Models.Game
             //{
             //    HomeTeamID = 2,
@@ -44,11 +45,55 @@ namespace Boccee_Ball_League
             //    Notes = "Ten points to Gryffindor!!!",
             //};
 
-            db.Games.Add(firstGame);
+            //db.Games.Add(firstGame);
             //db.Games.Add(secondGame);
             //db.Games.Add(thirdGame);
 
+            // Creating a bunch of teams.
+            //var teamA = new Models.Team
+            //{
+            //    Mascot = "Honeybadgers",
+            //    Color = "Dark Gray"
+            //};
+            //var teamB = new Models.Team
+            //{
+            //    Mascot = "Warriors",
+            //    Color = "Gold"
+            //};
+            //var teamC = new Models.Team
+            //{
+            //    Mascot = "Snakes",
+            //    Color = "Green"
+            //};
+            //var teamD = new Models.Team
+            //{
+            //    Mascot = "Eagles",
+            //    Color = "Brown"
+            //};
+            //var teamE = new Models.Team
+            //{
+            //    Mascot = "Griffins",
+            //    Color = "Silver"
+            //};
+            //var teamF = new Models.Team
+            //{
+            //    Mascot = "Tigers",
+            //    Color = "Orange"
+            //};
 
+            //db.Teams.Add(teamA);
+            //db.Teams.Add(teamB);
+            //db.Teams.Add(teamC);
+            //db.Teams.Add(teamD);
+            //db.Teams.Add(teamE);
+            //db.Teams.Add(teamF);
+
+            foreach(var team in Teams)
+            {
+                Console.WriteLine(team.Mascot);
+            }
+
+            Console.ReadLine();
             db.SaveChanges();
         }
     }
